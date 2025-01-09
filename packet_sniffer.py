1d93870561ce1f06d38958deec6fa473e2a853be
Mohamed-Yasser Houssein <mike.012555@gmail.com>
2025-01-09 16:26:34 -0500

    added conditions for DNS raw data
    Added if statements to prevent unreadable raw data to be assigned to the answer variable

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,6 +1,6 @@
 import threading
 from scapy.all import  sniff, wrpcap
-from scapy.layers.dns import DNS
+from scapy.layers.dns import DNS,UDP
 from collections import defaultdict
 #Define a list to store captured packets
 captured_packets = []
@@ -15,14 +15,31 @@ DNS_SPOOF_THRESHOLD = 3 #multiple IPs in a single DNS response
 def packet_callback(packet):
     # Append each captured packet to the list
     captured_packets.append(packet)
+    #print("**NEW PACKET** " + packet.show())  # Examine problematic packets
+
+    #ensuring UDP raw data handling
+    if packet.haslayer(UDP):  # Step 1
+        udp_payload = packet[UDP].payload  # Step 2
+        #if udp_payload:  # Step 3
+            #print(f"UDP Payload: {bytes(udp_payload)}")  # Step 4
 
     # Detect DNS spoofing
+
     if packet.haslayer(DNS) and packet[DNS].qr == 1: #DNS response
         domain = packet[DNS].qd.qname.decode() if packet[DNS].qd else "Unknown"
-        answers = [packet[DNS].an[i].rdata for i in range(packet[DNS].ancount)]
-        dns_tracker[domain].update(answers)
-        if(len(dns_tracker[domain])) > DNS_SPOOF_THRESHOLD:
-            print(f"\n[ALERT] Potential DNS spoofing detected for {domain}")
+        if packet[DNS].ancount > 0:
+            answers = []
+            for i in range(packet[DNS].ancount):
+                try:
+                    answer = packet[DNS].an[i].rdata
+                    answers.append(answer)
+                except AttributeError as e:
+                    print(f"Error accessing rdata in DNS answer: {e}")
+                    continue
+            # Update the tracker with the extracted answers
+            dns_tracker[domain].update(answers)
+            if(len(dns_tracker[domain])) > DNS_SPOOF_THRESHOLD:
+                 print(f"\n[ALERT] Potential DNS spoofing detected for {domain}")
 
 
 def sniff_packets():
